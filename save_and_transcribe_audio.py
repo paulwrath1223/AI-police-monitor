4ded7b1fc14f858b7fc395a0d0ef23456a8b7ecb
Paul Fornage <paulwrath123@gmail.com>
2022-11-10 14:21:27 -0800

    more work

---
+++ save_and_transcribe_audio.py
@@ -0,0 +1,37 @@
+# Save audio as wav file, named after current time and date
+import wave
+from datetime import datetime as dt
+import os
+
+folder_name = "transmission history"
+
+
+def transcribe(audio_file_name):
+    return "demo transcript"
+
+
+def save_and_transcribe_audio(audio, sample_width, sample_rate=44100, channels=1):
+    time = dt.now()
+    stringtime = time.strftime("%m-%d-%Y_%H-%M-%S")
+    file_base = folder_name + "/" + stringtime + "_police_message/"
+
+    os.mkdir(file_base)
+
+    audio_file_name = file_base + "audio recording.wav"
+    transcript_file_name = file_base + "transcript.txt"
+
+    wf = wave.open(audio_file_name, 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(sample_width)
+    wf.setframerate(sample_rate)
+    wf.writeframes(b''.join(audio))
+    wf.close()
+
+    transcript = transcribe(audio_file_name)
+
+    with open(transcript_file_name, 'w') as f:
+        f.write(transcript)
+    f.close()
+
+
+
--- testing.py
+++ testing.py
@@ -1,18 +1,18 @@
 import pyaudio
 import wave
 from is_silence import is_silence
-from save_audio import save_audio
+from save_and_transcribe_audio import save_and_transcribe_audio
 
 chunk = 1024  # Record in chunks of 1024 samples
 sample_format = pyaudio.paInt16  # 16 bits per sample
-# sample_format = pyaudio.paFloat32  # TEST
-channels = 2
+
+channels = 1
 fs = 44100  # Record at 44100 samples per second
 seconds = 3
 
 last_chunk_silent = True
 chunk_silent = True
-chunk_buffer = []
+chunk_buffer = bytearray()
 p = pyaudio.PyAudio()  # Create an interface to PortAudio
 
 info = p.get_host_api_info_by_index(0)
@@ -44,18 +44,23 @@ stream = p.open(format=sample_format,
 frames = []  # Initialize array to store frames
 sample_width = p.get_sample_size(sample_format)
 # Store data in chunks for 3 seconds
+
 for i in range(0, int(fs / chunk * seconds)):
     last_chunk_silent = chunk_silent
-    data = stream.read(chunk)
+    data = bytearray(stream.read(chunk))
     chunk_silent = is_silence(data)
     if chunk_silent:
         if last_chunk_silent:
             chunk_buffer = data
         else:
-            chunk_buffer.append(data)
-            save_audio(chunk_buffer, sample_width, sample_rate=fs, channels=channels)
+            chunk_buffer.extend(data)
+            save_and_transcribe_audio(chunk_buffer, sample_width, sample_rate=fs, channels=channels)
+
     else:
-        chunk_buffer.append(data)
+        chunk_buffer.extend(data)
+    # if i == 20:
+    #     for j in range(chunk):
+    #         print(data[j], end=",")
 
 
 # Stop and close the stream
@@ -64,13 +69,5 @@ stream.close()
 # Terminate the PortAudio interface
 p.terminate()
 
-print(p.get_sample_size(sample_format))
-print('Finished recording')
 
-# Save the recorded data as a WAV file
-# wf = wave.open(filename, 'wb')
-# wf.setnchannels(channels)
-# wf.setsampwidth(p.get_sample_size(sample_format))
-# wf.setframerate(fs)
-# wf.writeframes(b''.join(frames))
-# wf.close()
+print('Finished recording')
